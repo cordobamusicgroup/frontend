d2effdff05cc83851a65c3dbf881391a407da90b
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-26 19:45:15 -0300

    chore: Update ErrorModal component styling

--- src/components/molecules/ErrorModal.tsx
+++ src/components/molecules/ErrorModal.tsx
@@ -15,11 +15,13 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "80%",
+  maxWidth: 400,
   bgcolor: "background.paper",
   border: "1px solid #ccc",
   boxShadow: 24,
   p: 4,
+  borderRadius: 2,
 };
 
 const ErrorModal: React.FC<ErrorModalProps> = ({ open, onClose, errorMessage }) => {
