6b3b051f22dc6609964a7bad4208c064ee8ff1f2
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-25 21:54:58 -0300

    chore: Enable React strict mode and remove unused code in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,7 @@
-"use client";
-
 import "./globals.css";
 import theme from "@/theme";
-import { AuthProvider, useAuth } from "@/context/AuthContext";
+import { AuthProvider } from "@/context/AuthContext";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useRouter, usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-
-const AuthGuard = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (user === null && pathname !== "/auth") {
-      router.push("/auth");
-    } else {
-    }
-  }, [user, router, pathname]);
-
-  return <>{children}</>;
-};
 
 export default function RootLayout({
   children,
@@ -31,11 +12,9 @@ export default function RootLayout({
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AuthProvider>
-        <AuthGuard>
-          <html lang="en">
-            <body>{children}</body>
-          </html>
-        </AuthGuard>
+        <html lang="en">
+          <body>{children}</body>
+        </html>
       </AuthProvider>
     </ThemeProvider>
   );
