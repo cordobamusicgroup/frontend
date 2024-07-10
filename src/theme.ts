ad8f500b0b72e4b5167038ee1b50fd28f3f426db
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-07-10 00:31:32 -0300

    chore: Add breakpoints to theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -14,6 +14,15 @@ const theme = createTheme({
   typography: {
     fontFamily: "Roboto, Arial, sans-serif", // Tipografía
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
 
 export default theme;
