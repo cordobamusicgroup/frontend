4c9c963c97524c98a6a60eac94da03cc26ecbc85
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-09-26 19:42:01 -0300

    refactor: Remove filterItemsByRole function from portalMenuUtils.ts

--- src/lib/menus/portalMenuUtils.ts
+++
@@ -1,3 +0,0 @@
-export const filterItemsByRole = <T extends { roles: string[] }>(items: T[], role: string): T[] => {
-  return items.filter((item) => item.roles.includes("ALL") || item.roles.includes(role));
-};
